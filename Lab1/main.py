32609c5ae56e9d72056eab3c34b586b09e421fcb
Maistho <maistho@gmail.com>
2014-01-20 12:21:22 +0100

    added basic template extension

--- Lab1/main.py
+++ Lab1/main.py
@@ -9,7 +9,7 @@ app.jinja_env.line_comment_prefix = "##"
 
 @app.route('/')
 def hello_world():
-    return render_template("template1.html", title="Start page", names=["Gustav", "Filip"])
+    return render_template("start.html", title="Start page", names=["Gustav", "Filip"])
 
 @app.route('/<what>')
 def noname(what=None):
