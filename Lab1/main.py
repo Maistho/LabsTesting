6690449bf356c551765713fe16fc68f4a48fa919
Maistho <maistho@gmail.com>
2014-01-20 10:37:42 +0100

    Added basic template

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from jinja2 import Template
+import sys
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    template = Template(open("Templates/template1.html").read())
+    return template.render(name="Gustav")
 
 
 if __name__ == '__main__':
