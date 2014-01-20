056fd3a2194eca937959ca5fc13c1f46475b943c
Maistho <maistho@gmail.com>
2014-01-20 11:27:19 +0100

    Added line statements and line comments

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from jinja2 import Template
-import sys
+
 
 app = Flask(__name__)
-
+app.debug = True
+app.jinja_env.line_statement_prefix = '#'
+app.jinja_env.line_comment_prefix = "##"
 
 @app.route('/')
 def hello_world():
-    template = Template(open("Templates/template1.html").read())
-    return template.render(name="Gustav")
+    return render_template("template1.html", names=["Gustav", "Filip"])
 
 
 if __name__ == '__main__':
