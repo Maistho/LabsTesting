0022f4c1a7feea2f9618c84c419cfe0b1d77e717
Maistho <maistho@gmail.com>
2014-01-21 21:20:35 +0100

    Labb2

---
+++ Lab2/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, abort
+from jinja2 import Template
+
+
+app = Flask(__name__)
+app.debug = True
+app.jinja_env.line_statement_prefix = '#'
+app.jinja_env.line_comment_prefix = "##"
+files = ["favicon.ico", "style.css"]
+@app.route('/')
+def landing():
+    return render_template("landing.html", pythoncode=open("main.py").read())
+
+@app.route('/about')
+def about():
+    return render_template("about.html", gustavs_code=open("gustavskod.rb").read())
+
+@app.route('/register')
+def register():
+    people = "1 person has"
+    return render_template("register.html", people=people)
+
+@app.route('/<what>')
+def default(what=None):
+    if what in files:
+        return open(what).read()
+    else:
+        return abort(404)
+
+if __name__ == '__main__':
+    app.run()
